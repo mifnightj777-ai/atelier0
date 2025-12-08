32fd4336d3a8c40567e0723317b3e75d89b4bbd8
Yena Jang <yenajang@YenanoMacBook-Air.local>
2025-12-08 17:48:27 +0900

    update all pages design to City Pop style

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do 
+  root "fragments#index"
+  
   get 'users/show'
   devise_for :users
   resources :fragments
   get '/@:username', to: 'users#show', as: :user_profile
 
   get "up" => "rails/health#show", as: :rails_health_check
-  
-  root "static_pages#top"
-
 end
