f68188d924d81b0648fd05463628ffa71d50f8aa
Yena Jang <yenajang@YenanoMacBook-Air.local>
2025-12-09 21:16:55 +0900

    complete atelier0 with letter notifications

--- app/controllers/letters_controller.rb
+++ app/controllers/letters_controller.rb
@@ -17,7 +17,15 @@ class LettersController < ApplicationController
     @letter.sender = current_user
     @letter.recipient = @fragment.user
 
-    if @letter.save      
+    if @letter.save
+      if @letter.recipient != current_user
+        Notification.create(
+          recipient: @letter.recipient, # 受取人
+          sender: current_user,         # 送り主
+          action: "letter",             # 種類: letter
+          notifiable: @letter           # 対象: この手紙データ
+        )
+      end      
       redirect_to @fragment, notice: "Your letter has been sent quietly."
     else
       render :new, status: :unprocessable_entity
