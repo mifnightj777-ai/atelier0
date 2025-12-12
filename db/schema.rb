0790ccee4088ee288fc208d3565acf2b2bbba043
Yena Jang <yenajang@YenanoMacBook-Air.local>
2025-12-12 18:22:31 +0900

    Add visibility column to fragments table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_12_11_160244) do
+ActiveRecord::Schema[7.1].define(version: 2025_12_12_091554) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -93,6 +93,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_12_11_160244) do
     t.bigint "parent_id"
     t.bigint "root_id"
     t.bigint "prompt_id"
+    t.integer "visibility"
     t.index ["parent_id"], name: "index_fragments_on_parent_id"
     t.index ["prompt_id"], name: "index_fragments_on_prompt_id"
     t.index ["root_id"], name: "index_fragments_on_root_id"
