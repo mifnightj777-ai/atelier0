792ea8243dd83051317fab059cda7d8ca1f3fa6d
Yena Jang <mifnightj777@gmail.com>
2025-12-23 15:41:33 +0900

    Fix notification association name from actor to sender

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,7 +2,7 @@ class NotificationsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @notifications = current_user.notifications.includes(:actor, :notifiable).order(created_at: :desc) || []
+    @notifications = current_user.notifications.includes(:sender, :notifiable).order(created_at: :desc) || []
     current_user.notifications.unread.update_all(read_at: Time.current)
   end
 end
