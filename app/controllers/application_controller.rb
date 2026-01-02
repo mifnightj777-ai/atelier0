bc87cc365231a4e6305bff879313b67d99018a16
Yena Jang <mifnightj777@gmail.com>
2026-01-03 06:52:58 +0900

    update: 規約のモーダル化と未保存時のメモロック機能を追加 キャンセルについて

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
     before_action :authenticate_user!
     before_action :configure_permitted_parameters, if: :devise_controller?
 
+    def after_sign_in_path_for(resource)
+      root_path # ログイン後に「トップページ」に移動する設定
+    end
+
     protected
 
     def configure_permitted_parameters
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
+
+  skip_before_action :authenticate_user!, only: [:terms, :privacy]
+  
   def terms
   end
 
