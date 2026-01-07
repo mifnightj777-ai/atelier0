29a258cf699109d1a54fd2748e711fb4a5b0c015
Yena Jang <mifnightj777@gmail.com>
2026-01-07 17:48:36 +0900

    fix: fix read more and delete

--- app/javascript/controllers/read_more_controller.js
+++ app/javascript/controllers/read_more_controller.js
@@ -1,20 +1,8 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["content", "button"]
-
   connect() {
-    // 画面が表示された瞬間、文字が枠より溢れているかチェック
-    if (this.contentTarget.scrollHeight > this.contentTarget.clientHeight) {
-      // 溢れていればボタンを表示
-      this.buttonTarget.classList.remove("hidden")
-    }
-  }
-
-  expand() {
-    // クラスを外して全文表示にする
-    this.contentTarget.classList.remove("line-clamp-3")
-    // ボタンはもういらないので消す
-    this.buttonTarget.classList.add("hidden")
+    // この機能は現在停止中です。
+    // エラーを出さないために、中身を空にしています。
   }
 }
